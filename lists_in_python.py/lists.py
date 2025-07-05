fe5f05378adfb1ba4ae3f44ffcab3e1c847e653a
FarazCodeCraft <farazraja1026@gmail.com>
2025-07-05 16:26:11 -0700

    learning

---
+++ lists_in_python.py/lists.py
@@ -0,0 +1,21 @@
+# extend method add another list in one list
+# insert allow you to add or modify list using index numbers
+# remove are used to remove first item in list
+# clear makes your list complete empty
+# index() method allow you to find somethings's index
+# index() method also allow you to find somethings's index using range
+# reverse() method reverse list
+# sort() method allow us to arrange our list
+# pop()  
+"""
+         list joining
+1. + operator
+2. append method by using loop
+3. extend method ( list1.extend(list2))
+"""
+
+"""
+ List comprehensions
+syntax:
+list = [ expression FOR item IN iterable IF condition] 
+"""
